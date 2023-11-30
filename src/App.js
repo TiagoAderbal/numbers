65d6cab670a363796e0269853b1ad0799053279f
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-11-30 16:08:03 -0300

    fim do dia 30-11-2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import './components/navbar.js';
-import './components/section.js';
+import './components/about.js';
 import './components/footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/navbar.js';
-import Section from './components/section.js';
+import About from './components/about.js';
 import Footer from './components/footer.js';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <div className='container-fluid' id='banner'>
         <img className='container-fluid' src='./slide_dbsaude.jpg' alt=''></img>
       </div>
-      <Section></Section>
+      <About></About>
       <Footer></Footer>
     </div>
   );
--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './section.css';
+import './about.css';
 
-function Section() {
+function About() {
     return (
         <div className='container' id='about'>
             <div className='title-about'>
@@ -51,4 +51,4 @@ function Section() {
     );
 }
 
-export default Section;
+export default About;
