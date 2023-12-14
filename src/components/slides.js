d3dde1a29c82e5a2425c6bdfdfd28df8e04bc7fd
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-12-14 11:42:13 -0300

    adicionado carousel

---
+++ src/components/slides.js
@@ -0,0 +1,25 @@
+import './slides.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+function Slides() {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item interval={3000}>
+                    <img className='slide' src='./slide_dbsaude.jpg' alt=''></img>
+                    <Carousel.Caption>
+                        <p className='caption'>Tome decisões baseadas em dados.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img className='slide' src='./slide_hand1.jpg' alt=''></img>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+}
+
+export default Slides;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,17 +4,16 @@ import Navbar from '../../components/navbar.js';
 import About from '../../components/about.js';
 import Footer from '../../components/footer.js';
 import Button from '../../components/button.js';
+import Slides from '../../components/slides.js';
 
 function Home() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <div className='container-fluid' id='banner'>
-        <img className='container-fluid' src='./slide_dbsaude.jpg' alt=''></img>
-      </div>
-      <About></About>
-      <Button></Button>
-      <Footer></Footer>
+      <Navbar/>
+      <Slides/>
+      <About/>
+      <Button/>
+      <Footer/>
     </div>
   );
 }
